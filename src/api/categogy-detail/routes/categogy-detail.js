779541b27b4c4517bec4f23b8c5f55d3de862198
nvhoang2652 <nvhoang2605@gmail.com>
2023-03-22 22:58:08 +0700

    fix entity

--- src/api/categogy-detail/routes/categogy-detail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * categogy-detail router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::categogy-detail.categogy-detail');
